46b633f85a8b7bc17dd620829df88c7bff1737c7
st2007-coder <st5994@srmist.edu.in>
2026-03-02 09:31:16 +0530

    Add files via upload

---
+++ BannerApp.java
@@ -0,0 +1,48 @@
+package com.oops.banner;
+
+import java.util.Scanner;
+
+public class BannerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        BannerPrinter printer = new BannerPrinter();
+
+        System.out.println("---- OOPS Banner App ----");
+        System.out.println("1. Simple Display");
+        System.out.println("2. Basic Banner");
+        System.out.println("3. Advanced OOPS Banner");
+        System.out.print("Enter your choice: ");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine(); 
+
+        switch (choice) {
+
+            case 1:
+                printer.displaySimple();
+                break;
+
+            case 2:
+                printer.displayBasicBanner();
+                break;
+
+            case 3:
+                System.out.print("Enter text (Only O, P, S allowed): ");
+                String input = scanner.nextLine();
+
+                if (printer.validateInput(input)) {
+                    printer.displayBanner(input.toUpperCase());
+                } else {
+                    System.out.println("Invalid input! Only O, P, S allowed.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid choice!");
+        }
+
+        scanner.close();
+    }
+}
