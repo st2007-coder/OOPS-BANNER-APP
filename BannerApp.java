05a3dff9e76288150fe616b175e9ce96eecb0f1b
st2007-coder <st5994@srmist.edu.in>
2026-03-02 08:46:14 +0530

    Add files via upload

---
+++ BannerApp.java
@@ -0,0 +1,17 @@
+package com.oops.banner;
+
+public class BannerApp {
+
+    public static void main(String[] args) {
+
+        // UC2 - 7 Line Banner using manual print statements
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *****  ");
+        System.out.println("*     *  *     *  *     *       * ");
+        System.out.println("*     *  *     *  *     *       * ");
+        System.out.println(" *****    *****    *****    ***** ");
+    }
+}
